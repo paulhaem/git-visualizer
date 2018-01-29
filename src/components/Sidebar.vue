bbb6e5e9fa6645756150b72860d77386b8368fcf
Philipp Hamerle <phamerle.mmt-b2016@fh-salzburg.ac.at>
2018-01-29 17:20:32 +0100

    initial commit

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="sidebar">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Sidebar',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.sidebar {
+  width: 300px;
+  height: 700px;
+  background-color: #dddddd;
+  flex-shrink: 0;
+}
+</style>
