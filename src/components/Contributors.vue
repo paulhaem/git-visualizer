1667988b7fa2b0c3a368885c92d77e3dd7b82671
Philipp Hamerle <phamerle.mmt-b2016@fh-salzburg.ac.at>
2018-02-01 18:00:06 +0100

    feat: load commits in contributors tab

--- src/components/Contributors.vue
+++ src/components/Contributors.vue
@@ -1,14 +1,44 @@
 <template>
   <div class="contributors">
-    Contributers
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'Contributors',
-  mounted() {
+  computed: {
+    commits() {
+      return this.$store.getters.commits;
+    }
+  },
+  beforeMount() {
+    const repodata = {
+      owner: this.$route.params.username,
+      repo: this.$route.params.reponame,
+      token: this.$store.getters.token,
+    };
+    this.getCommits(repodata);
     this.$store.commit('setDisplayNavigation', true);
+  },
+  beforeDestroy() {
+    this.$store.commit('delCommits');
+  },
+  methods: {
+    getCommits(repodata) {
+      this.$store.dispatch('getCommits', repodata);
+    }
+  },
+  watch: {
+    // eslint-disable-next-line
+    '$route.params.reponame'(newReponame, oldReponame) {
+      const repodata = {
+        owner: this.$route.params.username,
+        repo: this.$route.params.reponame,
+      };
+      this.$store.commit('delCommits');
+      this.getCommits(repodata);
+    },
   },
 };
 </script>
