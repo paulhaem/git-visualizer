9c8c644689ff0896f82973b8f2cdd0447735110d
Alexander Hämmerle <alexhaem@gmail.com>
2018-02-02 10:20:50 +0100

    feat: add 404 page, add navigation guard for dashboard

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="pageNotFound">
+    <div class="card">
+      <div class="cardheader">
+        <h1>404 - Page not found</h1>
+      </div>
+      <div class="content">
+        <p class="robot">🤖</p>
+        <p>"This is not the page you were looking for. Head back to home or search for repositories on the top."</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageNotFound',
+  mounted() {
+    this.$store.commit('setDisplayNavigation', false);
+  },
+};
+</script>
+
+<style scoped>
+
+.card {
+  width: 100%;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16);
+  margin: 0 15px 15px 15px;
+  background-color: #ffffff;
+}
+.pageNotFound h1 {
+  color: white;
+  line-height: 1.5;
+  text-align: left;
+  padding-left: .5em;
+  margin-top: 0;
+}
+.cardheader {
+  border-radius: 5px 5px 0 0;
+  background-color: #845C9C;
+  position: relative;
+}
+.content {
+  font-size: 1.2em;
+  color: #888888;
+
+  padding: 0 1em .5em 1em;
+}
+
+.robot {
+  font-size: 2em;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
+import PageNotFound from '@/components/PageNotFound';
 import Results from '@/components/Results';
 import Information from '@/components/Information';
 import Commits from '@/components/Commits';
 import Contributors from '@/components/Contributors';
 import Dashboard from '@/components/Dashboard';
+
+import { store } from './../store/store';
+
 
 Vue.use(Router);
 
@@ -18,9 +22,24 @@ export default new Router({
       component: Home,
     },
     {
+      path: '*',
+      name: 'PageNotFound',
+      component: PageNotFound,
+      meta: { title: 'GitVisualizer - 404' },
+    },
+    {
       path: '/dashboard',
       name: 'Dashboard',
       component: Dashboard,
+      beforeEnter: (to, from, next) => {
+        if (store.getters.isAuthenticated) {
+          next();
+        } else {
+          next({
+            name: 'PageNotFound',
+          });
+        }
+      },
     },
     {
       path: '/repo/:username/:reponame/information',
