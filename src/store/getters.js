5548df00f8d07b5be8672f8645bdb647786b50a2
Philipp Hamerle <phamerle.mmt-b2016@fh-salzburg.ac.at>
2018-02-02 11:05:01 +0100

    feat: add filter for commitTypes getter

--- src/store/getters.js
+++ src/store/getters.js
@@ -32,4 +32,37 @@ export default {
   userrepos(state) {
     return state.userrepos;
   },
+  commitTypes(state) {
+    const types = {
+      chores: 0,
+      docs: 0,
+      feat: 0,
+      fix: 0,
+      refactor: 0,
+      style: 0,
+      test: 0,
+      notSemantic: 0,
+    };
+    const commits = state.commits;
+    commits.forEach((commit) => {
+      if (commit.commit.message.startsWith('chore:')) {
+        types.chores += 1;
+      } else if (commit.commit.message.startsWith('docs:')) {
+        types.docs += 1;
+      } else if (commit.commit.message.startsWith('feat:')) {
+        types.feat += 1;
+      } else if (commit.commit.message.startsWith('fix:')) {
+        types.fix += 1;
+      } else if (commit.commit.message.startsWith('refactor:')) {
+        types.refactor += 1;
+      } else if (commit.commit.message.startsWith('style:')) {
+        types.style += 1;
+      } else if (commit.commit.message.startsWith('test:')) {
+        types.test += 1;
+      } else {
+        types.notSemantic += 1;
+      }
+    });
+    return types;
+  },
 };
