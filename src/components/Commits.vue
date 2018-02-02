41e03dd04d19a053347d30bc3c5ed9c878f8a395
Alexander Hämmerle <alexhaem@gmail.com>
2018-02-02 12:29:09 +0100

    feat: add commits chart

--- src/components/Commits.vue
+++ src/components/Commits.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="commits" v-if="commits">
-    <p v-for="commit in commits" :key="commit.id">{{ commit.commit.message }}</p>
+    <div class="card">
+      <div class="cardheader">
+        <h1>Commits</h1>
+      </div>
+      <div class="content">
+        <div id="chart"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
+import c3 from 'c3';
+
 export default {
   name: 'Commits',
   computed: {
@@ -18,9 +28,30 @@ export default {
       repo: this.$route.params.reponame,
       token: this.$store.getters.token,
     };
-    console.log(repodata.token);
     this.getCommits(repodata);
     this.$store.commit('setDisplayNavigation', true);
+  },
+  mounted() {
+    c3.generate({
+      bindto: '#chart',
+      data: {
+        columns: [
+          ['Categories', 50, 100, 150, 200, 150, 100, 50]
+        ],
+        type: 'bar',
+      },
+      axis: {
+        x: {
+          type: 'category',
+          categories: ['chore', 'docs', 'feat', 'fix', 'refactor', 'style', 'test'],
+        }
+      },
+      bar: {
+        width: {
+          ratio: 0.5,
+        }
+      }
+    });
   },
   beforeDestroy() {
     this.$store.commit('delCommits');
@@ -42,8 +73,44 @@ export default {
     },
   },
 };
+
 </script>
 
 <style scoped>
+
+.commits {
+  width: calc(100vw - 300px - 20vw);
+}
+
+.card {
+  width: 100%;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16);
+  margin: 0 15px 15px 15px;
+  background-color: #ffffff;
+}
+
+.cardheader {
+  border-radius: 5px 5px 0 0;
+  background-color: #845C9C;
+  position: relative;
+}
+
+.commits h1 {
+  color: white;
+  line-height: 1.5;
+  text-align: left;
+  padding-left: .5em;
+  margin-top: 0;
+}
+
+.content {
+  padding: 0 1em .5em 1em;
+}
+
+#chart {
+  width: 100%;
+}
 
 </style>
